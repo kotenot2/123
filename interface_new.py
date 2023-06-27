5d162bba25b5e26633e6f25dd41ead12b54c69a0
kotenot2 <132300544+kotenot2@users.noreply.github.com>
2023-06-27 09:59:23 +0300

    Add files via upload

--- interface_new.py
+++ interface_new.py
@@ -34,62 +34,65 @@ class BotInterface():
                 if command == 'привет':
                     base_new.base.create_db(conn)
                     self.info = self.tools.get_profile_info(event.user_id)
-                    print(self.info)
-                    if self.info['city'] is None:
-                        self.info['city'] = self.get_city(event)
-                    elif self.info['bdate'] is None:
-                        self.info['bdate'] = self.get_bdate(event)
-                    elif self.info['sex'] is None:
-                        self.info['sex'] = self.get_sex(event)
                     self.message_send(event.user_id, f'Здравствуй, {self.info["name"]}, напиши поиск или далее')
 
                 elif command == 'поиск' or 'далее':
                     self.message_send(event.user_id, 'Начинаем искать!')
                     if self.users:
-                        user = self.users.pop()
-                        photos_user = self.tools.get_photos(user['id'])
-                        attachment = ''
-                        for photo in photos_user:
-                            attachment += f'photo{photo["owner_id"]}_{photo["id"]},'
-                    else:
+                        bot.if_none_info(event)
                         self.users = self.tools.search_users(self.info)
                         self.user = self.users.pop()
-                        print(self.user)
-                        # check_lists = base_new.base.select_profiles(conn, self.info['id'], self.user['id'])
-                        # for check_list in check_lists:
-                        #     print( )
                         while base_new.base.select_profiles(conn, self.info['id'], self.user['id']):
                             self.user = self.users.pop()
                             if len(self.users) == 0:
                                 break
-                        # while self.user['id'] == check_list[1]:
-                        #     self.user = self.users.pop()
-                            print(self.user)
 
                         photos_user = self.tools.get_photos(self.user['id'])
                         attachment = ''
                         for photo in photos_user:
                             attachment += f'photo{photo["owner_id"]}_{photo["id"]},'
+                        self.message_send(event.user_id,
+                                          f'Встречайте {self.user["name"]} ссылка : vk.com/id{self.user["id"]}',
+                                          attachment=attachment
+                                          )
+                    else:
+                        bot.if_none_info(event)
+                        self.users = self.tools.search_users(self.info)
+                        self.user = self.users.pop()
 
+                        # while base_new.base.select_profiles(conn, self.info['id'], self.user['id']):
+                        #     self.user = self.users.pop()
+                        #     if len(self.users) == 0:
+                        #         break
+
+                        photos_user = self.tools.get_photos(self.user['id'])
+                        attachment = ''
+                        for photo in photos_user:
+                            attachment += f'photo{photo["owner_id"]}_{photo["id"]},'
                             self.offset +=50
-                            # else:
-                            #     # offset += 1
-                            #     print('Проблема с таблицами')
                         self.message_send(event.user_id,
                                           f'Встречайте {self.user["name"]} ссылка : vk.com/id{self.user["id"]}',
                                           attachment=attachment
                                          )
                         base_new.base.insert_profiles(conn, event.user_id, self.user['id'], self.user['name'])
 
-                # elif command == 'заново':
-                #     base_new.base.delete_db(conn)
-                #     self.message_send(event.user_id, 'Таблицы очищены, можно начать поиск заново')
+                elif command == 'заново':
+                    base_new.base.delete_db(conn)
+                    self.message_send(event.user_id, 'Таблицы очищены, можно начать поиск заново')
 
                 elif command == 'пока':
                     self.message_send(event.user_id, 'До встречи!')
                     break
                 else:
                     self.message_send(event.user_id, 'Ошибка, начките заново')
+
+    def if_none_info(self, event):
+        if self.info['city'] is None:
+            self.info['city'] = self.get_city(event)
+        elif self.info['bdate'] is None:
+            self.info['bdate'] = self.get_bdate(event)
+        elif self.info['sex'] is None:
+            self.info['sex'] = self.get_sex(event)
 
     def get_bdate(self, event):
         if self.info['bdate'] is None:
